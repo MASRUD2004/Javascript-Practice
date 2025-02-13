45ed8a193d7171a0577b9a79d27840a883ff2be3
MASRUD2004 <masrudhasan@gmail.com>
2025-02-13 17:18:46 +0600

    first commit

---
+++ if-else.js
@@ -0,0 +1,11 @@
+const salary = 25000;
+const isBCS = true;
+const height=67;
+
+// if(salary>20000 && height>66){
+//     console.log( 'su....patro')
+// }
+// else{
+//     console.log("dur ho beta" )
+// }
+
