612400327f5a794681d710af5cc7b446084c893b
MASRUD2004 <masrudhasan@gmail.com>
2025-03-11 00:34:59 +0600

    Added file via upload
    i wanted to record my practice task to understand Javascript finely so uploaded the file here.

---
+++ Practice code/practice.js
@@ -0,0 +1,29 @@
+// const number= 25;
+// if(typeof number === 'number'){
+//     console.log('value is number')
+// }
+// else{
+//     console.log("value is not a number")
+// }
+
+// const numbers=[3,12,56,566,90]
+// const student={
+//     name:"hasan"
+//     job:'study'
+// }
+// console.log(typeof student)
+// console.log(Array.isArray(numbers))
+// console.log(isNaN('2'))
+// function sum(num1,num2){
+//     return num1+num2;
+// }
+// const result =sum(1,10)
+// console.log(result)
+
+
+// arrow function
+// const sum = (num1, num2) => num1 + num2;
+// console.log(sum(1, 12));
+
+const sum =(num1,num2)=>num1+num2;
+console.log()
